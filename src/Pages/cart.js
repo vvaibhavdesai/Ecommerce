00832f0f0983906fc7bab2aba4fa40744ad9c1a0
Vaibhav Desai <vaibhavdesai888@gmail.com>
2021-08-06 15:13:44 +0530

    added razorpay payment gateway and toast

--- src/Pages/cart.js
+++ src/Pages/cart.js
@@ -1,89 +1,72 @@
 import { useCart } from "../Context/CartContextProvider";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from './../Context/AuthProvider';
-import { useEffect } from 'react';
-import axios from "axios"
+import { useAuth } from "./../Context/AuthProvider";
+import { useEffect } from "react";
+import axios from "axios";
+import { CartTemplate } from "./CartTemplate";
+import { ToastContainer, toast } from "react-toastify";
 
-function CartTemplate({ item }) {
-  const { setCart } = useCart();
-
-
-  return (
-    <div>
-      <table className="cart_table">
-        <thead className="theadwa">
-          <tr>
-            <th></th>
-            <th></th>
-            <th>Product</th>
-            <th>price</th>
-            <th>quantity</th>
-            <th>subtotal</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <button
-                onClick={() =>
-                  setCart((prev) => prev.filter((items) => item !== items))
-                }
-              >
-                <i className="fa fa-trash"></i>
-              </button>
-            </td>
-            <td>
-              <img src={item.image} />
-            </td>
-            <td>`${item.name}`</td>
-            <td>
-              <span> ${`${item.price}`}</span>
-            </td>
-            <td className="td-quantity-btn">
-              <button
-                onClick={() =>
-                  setCart((prevState) =>
-                    prevState.map((items) =>
-                      items._id === item._id
-                        ? { ...items, quantity: item.quantity + 1 }
-                        : items
-                    )
-                  )
-                }
-              >
-                <i className="fa fa-plus"></i>
-              </button>
-              <span>{item.quantity}</span>
-              <button
-                disabled={item.quantity === 1}
-                onClick={() =>
-                  setCart((prevState) =>
-                    prevState.map((items) =>
-                      items._id === item._id
-                        ? { ...items, quantity: item.quantity - 1 }
-                        : items
-                    )
-                  )
-                }
-              >
-                <i className="fa fa-minus"></i>
-              </button>
-            </td>
-            <td>
-              <span>${item.quantity * item.price}</span>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
-}
 export function Cart() {
-  const { cart } = useCart();
+  const { cart,setCart } = useCart();
   const navigate = useNavigate();
   const reducer = (accumulator, cart) => {
     return accumulator + cart.price * cart.quantity;
   };
+  async function displayRazorpay(total) {
+    console.log(total,"arey total ayi kya ?")
+    try {
+      const { data } = await axios.post(
+        `https://mongoDBInventorySetup.vaibhavdesai888.repl.co/user/checkout`,
+        {
+          data: {
+            amount: total,
+          },
+        }
+      );
+      console.log(data.orderDetails,"res h kya ??")
+      const options = {
+        order_id:data.orderDetails.id,
+        amount:data.orderDetails.amount,
+        currency:data.orderDetails.currency,
+        key: "rzp_test_sXXRddEDPlPG8r",
+        name: "Khazana Cart",
+        description: "Test Transaction",
+        image: "https://example.com/your_logo",
+        handler: function (response) {
+          setCart([])
+          notify("Order Placed, Happy Shopping!")
+          
+        },
+        prefill: {
+          name: " testman kumar ",
+          email: "testman.kumar@example.com",
+          contact: "9999999999",
+        },
+      };
+      const paymentObject = new Razorpay(options);
+      paymentObject.open();
+    } catch (error) {
+      console.log(error.message,"yeh hai error bantai");
+    }
+  }
+
+  function loadRazorpay(total) {
+    const script = document.createElement("script");
+    script.src = "https://checkout.razorpay.com/v1/checkout.js";
+    document.body.appendChild(script);
+    script.onload = ()=>displayRazorpay(total);
+  }
+  const total = cart.reduce(reducer, 0);
+
+  const notify=(message)=>toast.dark(message, {
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  })
 
   const cartitle = () => {
     if (cart.length === 0) {
@@ -103,14 +86,16 @@ export function Cart() {
     } else {
       return (
         <div>
-          <h2>cart price:{cart.reduce(reducer, 0)}</h2>
+          <h2>cart price:{total}</h2>
         </div>
       );
     }
   };
   return (
     <div>
-      <div>{cartitle()}</div>
+      <div><span>{cartitle()}</span><span>{cart.length >0 && <button onClick={()=>loadRazorpay(total)} className="use-anywhere-btn checkout-btn">
+        Buy Now
+      </button>}</span></div>
       <div
         style={{ display: "flex", flexDirection: "column", flexWrap: "wrap" }}
       >
@@ -118,6 +103,17 @@ export function Cart() {
           <CartTemplate item={item} />
         ))}
       </div>
+      <ToastContainer
+            position="bottom-right"
+            autoClose={2000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
     </div>
   );
 }
